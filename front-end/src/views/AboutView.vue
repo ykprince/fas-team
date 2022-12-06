3fff55029184d58562ad9d9eb20d38726ffdafd7
ykprince <songkruise@naver.com>
2022-12-06 10:39:29 +0900

    create project

---
+++ front-end/src/views/AboutView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+  </div>
+</template>
+<script>
+import http from '../http-common'
+
+export default {
+  methods: {
+    retrieveUsers () {
+      http
+        .get('/api/hello')
+        .then(response => {
+          console.log(response.data)
+        })
+        .catch(e => {
+          console.log(e)
+        })
+    }
+  },
+  mounted () {
+    this.retrieveUsers()
+  }
+}
+</script>
