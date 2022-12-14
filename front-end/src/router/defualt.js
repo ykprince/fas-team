e409a0719dc988f5d37a47d316fc581db7137dea
김상훈 <53948297+Hoon-Hub@users.noreply.github.com>
2022-12-14 14:42:07 +0900

    router updated
    router 파일을 분리하도록 변경하였습니다.
    작성 방법은 내부 파일을 참고바랍니다!
    + 기존의 router 내용은 default.js 로 이동하였습니다.

---
+++ front-end/src/router/defualt.js
@@ -0,0 +1,14 @@
+import HomeView from '@/views/HomeView.vue'
+
+export default [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/about',
+    name: 'about',
+    component: () => import('../views/AboutView.vue')
+  }
+]
