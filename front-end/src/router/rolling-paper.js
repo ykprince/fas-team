eb633d5b64847ccd340130c6e6237b03402353ff
김상훈 <53948297+Hoon-Hub@users.noreply.github.com>
2022-12-15 10:49:47 +0900

    rollingpaper내용추가

--- front-end/src/router/rolling-paper.js
+++ front-end/src/router/rolling-paper.js
@@ -3,5 +3,10 @@ export default [
     path: '/rollingpaper',
     name: 'rollingpaper',
     component: () => import('@/views/RollingPaperList.vue')
+  },
+  {
+    path: '/add-rollingpaper',
+    name: 'addRollingpaper',
+    component: () => import('@/views/RollingPaperAddNew.vue')
   }
 ]
--- front-end/src/views/RollingPaperList.vue
+++ front-end/src/views/RollingPaperList.vue
@@ -14,92 +14,12 @@
 <script setup>
 // store.js에서 개인정보-롤링페이퍼정보 수신
 // 임시 데이터
-const rollingpaperObj = [
-  {
-    paperName: 'Fasol', // 롤링페이퍼 1개 obj
-    paperStyle: {}, // 스타일 설정
-    paperList: [
-      // 개별 items (다른사람들이 작성)
-      {
-        writter: '김상훈',
-        title: '안녕하세요. fasol에서 작성',
-        content: '올해도 수고하셨습니다. 내년에도 잘 부탁드려요.',
-        style: {},
-        hiddenYn: true
-      },
-      {
-        writter: '이용근',
-        title: '안녕하세요.',
-        content: '올해도 수고하셨습니다.',
-        style: {},
-        hiddenYn: false
-      },
-      {
-        writter: '홍샘이',
-        title: '안녕하세요. 샘이에요',
-        content: '올해도 수고하셨습니다.',
-        style: {},
-        hiddenYn: true
-      }
-    ]
-  },
-  {
-    paperName: 'Fasol', // 롤링페이퍼 1개 obj
-    paperStyle: {}, // 스타일 설정
-    paperList: [
-      // 개별 items (다른사람들이 작성)
-      {
-        writter: '김상훈',
-        title: '안녕하세요. fasol에서 작성',
-        content: '올해도 수고하셨습니다. 내년에도 잘 부탁드려요.',
-        style: {},
-        hiddenYn: true
-      },
-      {
-        writter: '이용근',
-        title: '안녕하세요.',
-        content: '올해도 수고하셨습니다.',
-        style: {},
-        hiddenYn: false
-      },
-      {
-        writter: '홍샘이',
-        title: '안녕하세요. 샘이에요',
-        content: '올해도 수고하셨습니다.',
-        style: {},
-        hiddenYn: true
-      }
-    ]
-  },
-  {
-    paperName: 'Fasol', // 롤링페이퍼 1개 obj
-    paperStyle: {}, // 스타일 설정
-    paperList: [
-      // 개별 items (다른사람들이 작성)
-      {
-        writter: '김상훈',
-        title: '안녕하세요. fasol에서 작성',
-        content: '올해도 수고하셨습니다. 내년에도 잘 부탁드려요.',
-        style: {},
-        hiddenYn: true
-      },
-      {
-        writter: '이용근',
-        title: '안녕하세요.',
-        content: '올해도 수고하셨습니다.',
-        style: {},
-        hiddenYn: false
-      },
-      {
-        writter: '홍샘이',
-        title: '안녕하세요. 샘이에요',
-        content: '올해도 수고하셨습니다.',
-        style: {},
-        hiddenYn: true
-      }
-    ]
-  }
-]
+import { useStore } from 'vuex'
+import { computed } from 'vue'
+
+const store = useStore()
+const rollingpaperObj = computed(() => store.state.rollingpaper.paperObj)
+console.log(rollingpaperObj)
 
 const rmThispaper = function (rpItem) {
   console.log(rpItem)
@@ -112,6 +32,7 @@ const rmThispaper = function (rpItem) {
   display: flex;
   padding: 2rem;
   border: 1px solid grey;
+  margin: 0 2rem;
 }
 
 h2 {
